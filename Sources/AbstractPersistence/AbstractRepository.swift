0abd573f79da10f7f0783cdfc12cf34034b8eaef
Oleksii Moiseenko <>
2020-03-17 14:42:19 +0200

    more methods;

--- Sources/AbstractPersistence/AbstractRepository.swift
+++ Sources/AbstractPersistence/AbstractRepository.swift
@@ -1,11 +1,23 @@
 import Foundation
 
+public struct FindRequest {
+    var skip: Int?
+    var limit: Int?
+    var sortDescriptors: [NSSortDescriptor]?
+    var predicates: [NSPredicate]
+}
+
 public protocol AbstractRepository {
+
     associatedtype T: Identifiable
+    func insert(value: T)
+    func insertMany(value: [T])
     func save(value: T)
+    func saveMany(value: [T])
     func remove(value: T)
-    func find(predicate: NSPredicate) -> [T]
+    func find(request: FindRequest) -> [T]
     func getById(id: T.Identifier) -> T?
     func remove(id: T.Identifier)
     func get(predicate: NSPredicate) -> T?
+    
 }
